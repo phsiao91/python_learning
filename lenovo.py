5e2421f8123da549d21153d9a008d7455cdc63fc
Raymond <phinganhsiao@gmail.com>
2021-01-30 16:57:59 -0500

    more practice on loops and addition of lists

---
+++ lenovo.py
@@ -0,0 +1,12 @@
+for item in 'Python':
+    print(item)
+for names in['Ray, Kobe, Loki', 'Nicole', 'Denny']:
+    print(names)
+for numbers in range(10):
+    print(numbers)
+for num in range(5, 10, 2):
+    print(num)
+total= 0
+for numb in [20]:
+    total = total + numb
+print(total)
---
+++ shopping.py
@@ -0,0 +1,14 @@
+prices = [10, 100, 10]
+
+total = 0
+for price in prices:
+    total = total + price
+print(f"Total: {total}")
+
+for numbers in [20, 30, 50]:
+    total = total + numbers
+print(total)
+
+for sum in [10, 100]:
+    total = total + sum
+print(total)
